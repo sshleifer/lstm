33b08215ddbe04e5282b5103af873413986557c9
sshleifer <sam@kensho.com>
2016-12-11 01:15:00 -0500

    bogus

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,2 @@
 alias gt="python code/gen_test.py"
-alias tr="python code/ptb_word_lm.py --data_path=data --model test --save_path=new_log/ --logdir=new_log/"
+alias tr="python code/ptb_word_lm.py --data_path=data --model test --save_path=new_log --logdir=new_log"
