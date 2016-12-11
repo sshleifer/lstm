fd2198fce08b03c3fa33c4946557b449caabbc1e
sshleifer <sam@kensho.com>
2016-12-11 13:13:39 -0500

    fix PYTHONPATH annoyance

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,8 @@
 alias gt="python code/gen_test.py"
 alias tr="python code/ptb_word_lm.py --data_path=data --model test --save_path=new_log --logdir=new_log"
+
+trun (){
+    python code/ptb_word_lm.py --data_path=data --model $1 --save_path=$2 --logdir=new_log
+}
+
+export PYTHONPATH='.'
